5fcb65c4ecc25dc7f338d25a5a90da8dad8f07ff
Hafedh-Jendoubi <Hafedh.Jendoubi@esprit.tn>
2025-04-07 19:27:23 +0100

    Added multiple files to assure the functionality of Reclamation Service

--- backend/reclamation-management/src/main/java/com/esprit/microservice/reclamationmanagement/TestRepository.java
+++
@@ -1,6 +0,0 @@
-package com.esprit.microservice.reclamationmanagement;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity, Long> {
-}
